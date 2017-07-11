2fbdc4de4dd83e201a6c5d189c0a9481ce587abe
cherishgad <cherishgad@outlook.com>
2017-07-11 10:00:08 +0900

    commit find_file.py commit pandas_frame_class.py

--- find_file.py
+++ find_file.py
@@ -1,28 +1,12 @@
 import os.path
 import pandas as pd
-import pandas_frame_class
+import files_read_class
 AVA_path = '/home/cherishgad/ava/AVA.txt'
 image_path = '/home/cherishgad/AVA/images/'
 Style_path = '/home/cherishgad/ava/style.txt'
 
-def check_files(files_path, files_array, files_format):
-	count = 0
-	try:
-		for file_name in files_array:
-			if not os.path.exists(files_path + str(file_name) + files_format):
-				count+=1
-				
-	except:
-		print "wrong file_array"
-		return -1
-	
-	return count
 if __name__=="__main__":
-	AVA_frame = pandas_frame_class.pandas_frame(AVA_path)
-	AVA_frame.csv_to_frame()
-	AVA_frame.print_frame()
-	AVA_frame.frame_to_matrix()
-	AVA_frame.print_matrix()
-	matrix_data = AVA_frame.extract_matrix_column(1)
-	print check_files(image_path, matrix_data, ".jpg")
-
+	AVA_images = files_read_class.files_read(AVA_path)
+	AVA_images.print_frame()
+	AVA_images.print_matrix()
+	print AVA_images.check_files(image_path, 1, ".jpg")
--- pandas_frame_class.py
+++ pandas_frame_class.py
@@ -1,17 +1,17 @@
 import os.path
 import pandas as pd
-AVA_path = '/home/cherishgad/ava/AVA.txt'
-image_path = '/home/cherishgad/AVA/images/'
-Style_path = '/home/cherishgad/ava/style.txt'
-#with open(AVA_path) as fp:
-#	for line in fp:
-#		print line
+
 class pandas_frame:
 	def __init__(self, file_path):
 		self.file_path = file_path
 		self.is_frame = False
 		self.is_matrix = False
 	
+	def set_the_file_path(self, file_path):
+		self.file_path = file_path
+		self.is_frame = False
+		self.is_matrix = False
+
 	def csv_to_frame(self):
 		try:#check the file is exist
 			data = open(self.file_path)
@@ -77,26 +77,4 @@ class pandas_frame:
 		else:
 			print 'matrix is not exist'
 			return [-1]
-
-def check_files(files_path, files_array, files_format):
-	count = 0
-	try:
-		for file_name in files_array:
-			if not os.path.exists(files_path + str(file_name) + files_format):
-				count+=1
-				
-	except:
-		print "wrong file_array"
-		return -1
-	
-	return count
-			
-if __name__=="__main__":
-	AVA_frame = pandas_frame(AVA_path)
-	AVA_frame.csv_to_frame()
-	AVA_frame.print_frame()
-	AVA_frame.frame_to_matrix()
-	AVA_frame.print_matrix()
-	matrix_data = AVA_frame.extract_matrix_column(1)
-	print check_files(image_path, matrix_data, ".jpg")
 
