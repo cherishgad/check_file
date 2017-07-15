f9304bcc5bed2e7db1aa853912b07dc33d6e1c5e
cherishgad <cherishgad@outlook.com>
2017-07-15 19:47:37 +0900

    commit find_file.py

--- find_file.py
+++ find_file.py
@@ -1,12 +1,42 @@
 import os.path
+import os
 import pandas as pd
 import files_read_class
+style_path = '/home/cherishgad/ava/style_image_lists/'
+aesthetic_path = '/home/cherishgad/ava/aesthetics_image_lists/'
+
 AVA_path = '/home/cherishgad/ava/AVA.txt'
 image_path = '/home/cherishgad/AVA/images/'
-Style_path = '/home/cherishgad/ava/style.txt'
 
+def search(dirname):
+	file_name_list = []
+	filenames = os.listdir(dirname)
+	for filename in filenames:
+		full_filename = os.path.join(dirname, filename)
+		ext = os.path.splitext(full_filename)[-1]
+		if ext == ".jpgl":
+			file_name_list.append(full_filename)
+	return file_name_list
 if __name__=="__main__":
 	AVA_images = files_read_class.files_read(AVA_path)
-	AVA_images.print_frame()
-	AVA_images.print_matrix()
+	print AVA_path
 	print AVA_images.check_files(image_path, 1, ".jpg")
+	
+	aesthetic_list = search(aesthetic_path)
+ 
+	for aesthetic_path in aesthetic_list:
+		aesthetic_images = files_read_class.files_read(aesthetic_path)
+		print aesthetic_path
+		print aesthetic_images.check_files(image_path, 0,".jpg")
+	
+	
+	style_list = search(style_path)
+	
+	for style_path in style_list:
+		style_images = files_read_class.files_read(style_path)
+		print style_path
+		print style_images.check_files(image_path, 0,".jpg")
+
+
+
+
